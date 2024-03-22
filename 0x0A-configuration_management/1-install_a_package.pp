98522c31de0315ddbd56fed84dc112427bff6903
Kingsley <louis.officialz@gmail.com>
2024-03-22 14:02:33 -0700

    Configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,12 @@
-# Using a puppet manifest to install flask v2.1.0 from pip3.
+# Using a puppet to install flask v2.1.0 from pip3.
 # Specifying the version of flask to install using the ensure attribute.
-package { 'flask':
+package { 'Flask':
   ensure   => '2.1.0',
-  provider => pip3,
+  provider => 'pip3'
+}
+
+# Install a specific version of Werkzeug (2.1.1)
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
 }
