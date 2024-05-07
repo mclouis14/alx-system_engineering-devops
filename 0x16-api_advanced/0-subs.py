fc4ef5e19519cd2e7a7b287a2110cd46888f58ea
root <louis.officialz@gmail.com>
2024-05-07 23:34:49 +0000

    Api advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Function that queries number of subscribers on a given Reddit subreddit."""
+
 import requests
 
 
@@ -10,7 +11,7 @@ def number_of_subscribers(subreddit):
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    if response.status_code == 200:
         return 0
     results = response.json().get("data")
     return results.get("subscribers")
